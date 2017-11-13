1182a055bd6ba6e25c21c8ea9527251065ea08c7
rachel pehrsson <rhp8rg@virginia.edu>
2017-11-13 04:05:27 -0500

    beginning

---
+++ app/build/generated/source/buildConfig/debug/com/example/rhp8r/final_project_final_spearow/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.rhp8r.final_project_final_spearow;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.rhp8r.final_project_final_spearow";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
---
+++ app/src/main/java/com/example/rhp8r/final_project_final_spearow/LanguagesActivity.java
@@ -0,0 +1,13 @@
+package com.example.rhp8r.final_project_final_spearow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LanguagesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_languages);
+    }
+}
