51272e688b80dbeb22c483e8c5f8b251a2d5845b
shivyn21 <shivyn21@gmail.com>
2017-11-13 00:00:37 -0500

    Create Language.java

---
+++ app/src/main/java/com/example/rhp8r/final_project_final_spearow/Language.java
@@ -0,0 +1,39 @@
+package com.example.rhp8r.final_project_final_spearow;
+import android.support.v7.app.AppCompatActivity;
+import java.util.ArrayList;
+import android.widget.CheckBox;
+
+public class Language extends AppCompatActivity implements Comparable<Language> {
+    String lname;
+    ArrayList <String> words;
+    ArrayList <String> defs;
+    CheckBox f;
+    public Language() {
+        this.lname = "";
+        this.words = new ArrayList <String>();
+        this.defs = new ArrayList <String>();
+    }
+    public Language (String langname){
+        this.lname = langname;
+        this.words = new ArrayList <String>();
+        this.defs = new ArrayList <String>();
+    }
+    public String getLname(){
+        return this.lname;
+    }
+    public ArrayList <String> getWordList(){
+        return this.words;
+    }
+    public ArrayList <String> getDefList(){
+        return this.defs;
+    }
+
+    public static ArrayList<Language> createInitialBucketList(){ ///????? idk how this is actually supposed to work
+        ArrayList<Language> ls = new ArrayList<Language>();
+        ls.add(new Language("Spanish"));
+        ls.add(new Language("French"));
+
+        return ls;
+    }
+    //   This class should have a static method called createInitialBucketList() that creates the initial, hard-coded bucket list ArrayList and returns it. This will be called in the onCreate() in the BucketListActivity.
+}
