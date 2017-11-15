a54ad5d71f795ed9a38fa34ca71d903f8c0501ae
shivyn21 <shivyn21@gmail.com>
2017-11-14 19:41:39 -0500

    halp

--- app/src/main/java/com/example/rhp8r/final_project_final_spearow/AddLanguage.java
+++ app/src/main/java/com/example/rhp8r/final_project_final_spearow/AddLanguage.java
@@ -14,16 +14,15 @@ import android.widget.EditText;
  * Created by rhp8r on 11/14/2017.
  */
 
-public class AddLanguage extends AppCompatActivity{
-<<<<<<< HEAD
+public class AddLanguage extends AppCompatActivity {
+
     EditText addLanguage;
-=======
-EditText addLanguage;
->>>>>>> aa129db191456bd8772e00abdf7c0932202dd730
-    public boolean onSupportNavigateUp(){
+
+    public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
     }
+
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             // Respond to the action bar's Up/Home button
@@ -33,6 +32,7 @@ EditText addLanguage;
         }
         return super.onOptionsItemSelected(item);
     }
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
@@ -41,16 +41,13 @@ EditText addLanguage;
         setContentView(R.layout.add_language);
         addLanguage = (EditText) findViewById(R.id.newLang);
     }
+
     public void sendMessage(View view) {
         Intent intent2 = new Intent();
         Bundle b = new Bundle();
         b.putString("newLang", addLanguage.getText().toString());
         intent2.putExtras(b);
-        setResult(RESULT_OK,intent2);
+        setResult(RESULT_OK, intent2);
         finish();
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> aa129db191456bd8772e00abdf7c0932202dd730
