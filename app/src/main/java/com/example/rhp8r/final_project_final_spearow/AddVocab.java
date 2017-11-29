74f24cbe751018a2861a953b2bdef2c818d710b7
rachel pehrsson <rhp8rg@virginia.edu>
2017-11-29 04:24:23 -0500

    eh

--- app/src/main/java/com/example/rhp8r/final_project_final_spearow/AddVocab.java
+++ app/src/main/java/com/example/rhp8r/final_project_final_spearow/AddVocab.java
@@ -1,8 +1,13 @@
 package com.example.rhp8r.final_project_final_spearow;
 
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by rhp8r on 11/28/2017.
  */
 
-public class AddVocab {
+
+public class AddVocab extends AppCompatActivity {
+
+
 }
